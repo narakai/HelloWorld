4dbb1436b40eb60c66f85a53bde02ab07dd4f684
laileon <leon@igola.com>
2017-07-27 18:05:41 +0800

    uodated

--- index.android.js
+++ index.android.js
@@ -8,34 +8,65 @@ import React, {Component} from 'react';
 import {
     AppRegistry,
     StyleSheet,
-    Text,
+    Text, TouchableHighlight,
     View
 } from 'react-native';
 
 export default class HelloWorld extends Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Welcome to React Native!
-                </Text>
+            <View style={viewStyles}>
+                <TouchableHighlight
+                    style={[buttonStyles.core, buttonStyles.spacer]}
+                >
+                    <Text>Default Normal</Text>
+                </TouchableHighlight>
+                <TouchableHighlight
+                    style={[buttonStyles.core, buttonStyles.hairlineBorder,
+                        buttonStyles.spacer]}
+                >
+                    <Text>Default Hairline</Text>
+                </TouchableHighlight>
+                <TouchableHighlight
+                    style={[buttonStyles.core, buttonStyles.primary, buttonStyles.spacer]}
+                >
+                    <Text>Primary Normal</Text>
+                </TouchableHighlight>
+                <TouchableHighlight
+                    style={[buttonStyles.core, buttonStyles.primary,
+                        buttonStyles.hairlineBorder]}
+                >
+                    <Text>Primary Hairline</Text>
+                </TouchableHighlight>
             </View>
         );
     }
 }
+const viewStyles = {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+};
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'orange',
+const buttonStyles = StyleSheet.create({
+    core: {
+        borderStyle: 'solid',
+        borderColor: '#d5d5d5',
+        borderWidth: 1,
+        backgroundColor: '#eee',
+        borderRadius: 3,
+        padding: 3,
+        paddingLeft: 5,
+        paddingRight: 5
+    }, primary: {
+        backgroundColor: '#60b044',
+        borderColor: '#355f27'
     },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
+    hairlineBorder: {
+        borderWidth: StyleSheet.hairlineWidth
+    }, spacer: {
+        marginBottom: 10
+    }
 });
 
 AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
