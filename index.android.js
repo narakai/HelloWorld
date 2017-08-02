b3f9d9933f2798f30657b6d7d2419f1d29c37fb7
laileon <leon@igola.com>
2017-08-02 14:34:29 +0800

    add scroll view

--- index.android.js
+++ index.android.js
@@ -5,12 +5,7 @@
  */
 
 import React, {Component} from 'react';
-import {
-    AppRegistry, Image,
-    StyleSheet,
-    Text, TouchableHighlight,
-    View
-} from 'react-native';
+import {AppRegistry, ScrollView, StyleSheet, Text, TouchableHighlight, View} from 'react-native';
 import BoxModel from "./src/BoxModel";
 
 const pizzaImage = require('./src/img/pizza.jpg');
@@ -18,8 +13,7 @@ const pizzaImage = require('./src/img/pizza.jpg');
 export default class HelloWorld extends Component {
     render() {
         return (
-            <Image source={pizzaImage} style={buttonStyles.bg}>
-
+            <ScrollView>
                 <View style={viewStyles}>
                     <TouchableHighlight
                         style={[buttonStyles.core, buttonStyles.spacer]}
@@ -47,11 +41,9 @@ export default class HelloWorld extends Component {
                     >
                         <Text>Primary Hairline</Text>
                     </TouchableHighlight>
-
                     <BoxModel/>
                 </View>
-
-            </Image>
+            </ScrollView>
         );
     }
 }
@@ -110,6 +102,18 @@ const buttonStyles = StyleSheet.create({
         bottom: 0,
         left: 0,
         resizeMode: 'cover'
+    },
+});
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 22
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
     },
 });
 
